28108e5ca4d5686a79212b788ae823650042b83b
Bojana Sofronovic <bojanasofronovic@gmail.com>
2016-02-09 20:27:07 +0100

    Dodati boja i ugao kao atributi za oblik, i promenjeno da kafic ima 1 ili vise nivoa (makar prizemlje)

--- JSDProject/generation/languageInterpreter.py
+++ JSDProject/generation/languageInterpreter.py
@@ -8,18 +8,18 @@ class Interpreter(object):
         
     def interpret(self):
         
-        print (self.model.naziv_kafica)
+        print (self.model.naziv)
         print ('#####')
         for nivo in self.model.nivo:
-            print(nivo.naziv_nivoa)
+            print(nivo.naziv)
         print ('#####')
         for nivo in self.model.nivo:
-            print(nivo.naziv_nivoa+" : \n")
+            print(nivo.naziv+" : \n")
             for element in nivo.element:
-                print ("\t"+ element.naziv_elementa)
+                print ("\t"+ element.naziv)
             print ('#####') 
             for element in nivo.element:
-                print (element.naziv_elementa+" : \n")
+                print (element.naziv+" : \n")
                 for oblik in element.oblik:
                     print ("\t"+ oblik.__class__.__name__)
         print ('#####')
